27c07da509b354d25b9af7e22a61cea44792dfea
mkuzman <mkuzman@foi.hr>
2022-06-30 06:37:11 +0200

    Uređivanje početne forme (Form1)
    Dodavanje dva gumba za prijavu i pregled asortimana, preimenovanje forme u "Glavni Izbornik".

--- Software/NaruciISjedi/NaruciISjedi/Form1.cs
+++ Software/NaruciISjedi/NaruciISjedi/Form1.cs
@@ -10,11 +10,23 @@ using System.Windows.Forms;
 
 namespace NaruciISjedi
 {
-    public partial class Form1 : Form
+    public partial class PocetnaForma : Form
     {
-        public Form1()
+        public PocetnaForma()
         {
             InitializeComponent();
+        }
+
+        private void prijaviButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void asortimanButton_Click(object sender, EventArgs e)
+        {
+            AsortimanForma forma = new AsortimanForma();
+            forma.Show();
+
         }
     }
 }
