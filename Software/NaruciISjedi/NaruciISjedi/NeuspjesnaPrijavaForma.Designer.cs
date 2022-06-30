c878849480f49c747edbc8a22df7e3e92bf398cf
mkuzman <mkuzman@foi.hr>
2022-06-30 06:31:13 +0200

    Kreiranje forme NeuspjesnaPrijavaForma.
    Kreiranje forme, uređivanje (dodavanje gumba i teksta).

---
+++ Software/NaruciISjedi/NaruciISjedi/NeuspjesnaPrijavaForma.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace NaruciISjedi
+{
+    partial class NeuspjesnaPrijavaForma
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.okButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // okButton
+            // 
+            this.okButton.Location = new System.Drawing.Point(139, 122);
+            this.okButton.Name = "okButton";
+            this.okButton.Size = new System.Drawing.Size(75, 23);
+            this.okButton.TabIndex = 0;
+            this.okButton.Text = "OK!";
+            this.okButton.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(119, 66);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(127, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Nesupješna prijava!";
+            // 
+            // NeuspjesnaPrijavaForma
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(354, 229);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.okButton);
+            this.Name = "NeuspjesnaPrijavaForma";
+            this.Text = "NeuspjesnaPrijavaForma";
+            this.Load += new System.EventHandler(this.NeuspjesnaPrijavaForma_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button okButton;
+        private System.Windows.Forms.Label label1;
+    }
+}
