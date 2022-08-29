3f4f86128f58a7287623f7f1d6d1f1bf6fe56416
mkuzman <80122540+mkuzman1@users.noreply.github.com>
2022-08-29 10:55:00 +0200

    Pokušaj ispravljanja greškeCreate novo

---
+++ Software/NaručiISjedi/NaruciISjedi/IzradaIzvjescaForma.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NaruciISjedi
+{
+    public partial class IzradaIzvjescaForma : Form
+    {
+        private User korisnik;
+
+        public IzradaIzvjescaForma(User user)
+        {
+            InitializeComponent();
+            korisnik = user;
+        }
+
+        private void vratiAsortimanButton_Click(object sender, EventArgs e)
+        {
+            AsortimanForma form = new AsortimanForma(korisnik);
+            this.Hide();
+            form.ShowDialog();
+        }
+
+        private void prikaziIzvjesceButton_Click(object sender, EventArgs e)
+        {
+            IspisIzvjescaForma form = new IspisIzvjescaForma(korisnik);
+            this.Hide();
+            form.ShowDialog();
+        }
+        public void OdabraniPodatci()
+        {
+            string datum = datumTextBox.Text;
+            string vremenskiPeriod = periodGroupBox.Text;
+            
+        }
+
+        private void IzradaIzvjescaForma_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
