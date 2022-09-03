17e3abdd30cafb3cce62282aee41925995d34249
mkuzman <mkuzman@foi.hr>
2022-09-03 09:50:25 +0200

    Promjena na formi za ispis izvješća Dodavanje mogućnosti za ispis u pdf formatu

--- Software/NaručiISjedi/NaruciISjedi/IspisIzvjescaForma.cs
+++ Software/NaručiISjedi/NaruciISjedi/IspisIzvjescaForma.cs
@@ -49,24 +49,47 @@ namespace NaruciISjedi
 
         private void ispisiIzvjesceButton_Click(object sender, EventArgs e)
         {
+            List<Order> podatciZaIspis = DohvacanjePodataka();
             Document document = new Document();
             Page page = new Page();
             document.Pages.Add(page);
-            document.Draw("//Downloads"); //putanja je kriva
+            ceTe.DynamicPDF.PageElements.Table2 table2 = new ceTe.DynamicPDF.PageElements.Table2(0, 140, 600, 600);
+            table2.Columns.Add(180);
+            table2.Columns.Add(60);
+            table2.Columns.Add(60);
+            table2.Columns.Add(80);
+            table2.Columns.Add(90);
+            ceTe.DynamicPDF.PageElements.Row2 row1 = table2.Rows.Add(35, ceTe.DynamicPDF.Font.HelveticaBold, 12, Grayscale.Black,
+           Grayscale.Gray);
+            foreach (var item in podatciZaIspis)
+            {
+                ceTe.DynamicPDF.PageElements.Row2 row2 = table2.Rows.Add(25, ceTe.DynamicPDF.Font.HelveticaBold, 12, Grayscale.Black,
+           Grayscale.Gray);
+                row2.Cells.Add(item.IDProizvod.ToString());
+                row2.Cells.Add(item.Product.ToString());
+                row2.Cells.Add(item.kolicina.ToString());
+            }
+            document.Draw("C:\\Users\\Korisnik\\Desktop\\Izvjesce.pdf");
+            string path = "C:\\Users\\Korisnik\\Desktop\\Izvjesce.pdf";
         }
 
         private void IspisIzvjescaForma_Load(object sender, EventArgs e)
         {
-            using(var context = new PI2241_DBEntities1())
+            
+                orderBindingSource.DataSource = DohvacanjePodataka();
+            this.reportViewer1.RefreshReport(); //hvata datasource i prikazuje izvješće
+            
+        }
+        private List<Order> DohvacanjePodataka()
+        {
+            List<Order> narudzbaLista;
+            using (var context = new PI2241_DBEntities1())
             {
                 var query = from a in context.Orders
                             orderby a.kolicina descending
                             select a;
-                List<Order> narudzbaLista = query.ToList();
-                orderBindingSource.DataSource = narudzbaLista;
+                return narudzbaLista = query.ToList();
             }
-            this.reportViewer1.RefreshReport(); //hvata datasource i prikazuje izvješće
-            
         }
     }
 }
